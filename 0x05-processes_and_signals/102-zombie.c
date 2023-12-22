e3bb27bf636715aef2394e156cd70cdc54d89ef4
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-23 01:46:18 +0300

    Use redirect trick

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - program that creates 5 zombie processes
+ *
+ * Return: 0 as exit status
+ */
+int main(void)
+{
+	return (0);
+}
+
+/**
+ * infinite_while - infinite loop that pause for 1 sec
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
