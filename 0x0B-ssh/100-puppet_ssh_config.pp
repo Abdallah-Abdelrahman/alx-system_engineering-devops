7720c68de4d35fd9f089997f979ac643a049f05f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-25 08:30:02 +0300

    Use absolute path for home dir

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 # ssh configuration file
 file { 'config':
-  path    => "${facts['user_home']}/.ssh/config",
+  path    => "/home/ubuntu/.ssh/config",
   content => "
 	Host *
 	    IdentityFile ~/.ssh/school
 	    PasswordAuthentication no
 ",
   owner   => 'root',
+  group   => 'root',
   mode    => '0644'
 }
