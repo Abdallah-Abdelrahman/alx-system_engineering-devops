8b2eb012ad1c00d2ba9d0e228153e0fbed817930
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-27 21:52:25 +0300

    Debug checker 011-7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,49 +5,50 @@
 # The redirection must be a “301 Moved Permanently”
 # Your answer file should be a Puppet manifest containing commands to automatically,
 # configure an Ubuntu machine to respect above requirements
-
-package {'nginx': ensure => installed}
-
-file {'/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hellow World!',
-  require => Package['nginx'],
+class nginx {
+  package {'nginx':
+    ensure => installed,
+  }
+  file {'/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hellow World!',
+    require => Package['nginx'],
+  }
+  file {'/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => "server {
+  
+  	listen 80 default_server;
+  	listen [::]:80 default_server;
+  
+  	root /var/www/html;
+  
+  	# Add index.php to the list if you are using PHP
+  	index index.html index.htm index.nginx-debian.html;
+  
+  	server_name _;
+  
+  	location / {
+  		# First attempt to serve request as file, then
+  		# as directory, then fall back to displaying a 404.
+  		try_files \$uri \$uri/ =404;
+  	}
+  	location /redirect_me {
+  		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  	}
+  	error_page 404 /404.html;
+          location = /404.html {
+  		root /var/www/html;
+                  internal;
+          }
+  
+   }",
+    require => Package['nginx'],
+  }
+  service {'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
 }
-
-file {'/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "server {
-
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	root /var/www/html;
-
-	# Add index.php to the list if you are using PHP
-	index index.html index.htm index.nginx-debian.html;
-
-	server_name _;
-
-	location / {
-		# First attempt to serve request as file, then
-		# as directory, then fall back to displaying a 404.
-		try_files \$uri \$uri/ =404;
-	}
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-	error_page 404 /404.html;
-        location = /404.html {
-		root /var/www/html;
-                internal;
-        }
-
- }",
-  require => Package['nginx'],
-}
-
-service {'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
+include nginx
