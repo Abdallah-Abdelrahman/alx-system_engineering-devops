adb928a175802a1e60534d8fe6d338156d5ed8ca
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-05 02:50:55 +0300

    Use `file_line` resource type

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,28 +2,10 @@
 
 package {'haproxy': ensure => installed}
 
-file {'/etc/nginx/sites-enabled/default':
+file_line {'/etc/nginx/sites-enabled/default':
   ensure  => present,
-  content => "
-	server {
-		listen 80 default_server;
-		listen [::]:80 default_server;
-
-		add_header X-Served-By ${hostname};
-		root /var/www/html;
-	
-		# Add index.php to the list if you are using PHP
-		index index.html index.htm index.nginx-debian.html;
-
-		server_name _;
-	
-		location / {
-			# First attempt to serve request as file, then
-			# as directory, then fall back to displaying a 404.
-			try_files \$uri \$uri/ =404;
-		}
-	}
-",
+  after   => 'server {',
+  match   => "add_header X-Served-By ${hostname};",
   require => Package['nginx'],
 }
 
