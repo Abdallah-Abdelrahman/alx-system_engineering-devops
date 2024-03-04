796504726c303579232ef4cf02fd6f9041b35f4c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-05 02:29:50 +0300

    Append semi-colon

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,24 +5,24 @@ package {'haproxy': ensure => installed}
 file {'/etc/nginx/sites-enabled/default':
   ensure  => present,
   content => "
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	add_header X-Served-By ${hostname}
-	root /var/www/html;
-
-	# Add index.php to the list if you are using PHP
-	index index.html index.htm index.nginx-debian.html;
-
-	server_name _;
-
-	location / {
-		# First attempt to serve request as file, then
-		# as directory, then fall back to displaying a 404.
-		try_files \$uri \$uri/ =404;
+	server {
+		listen 80 default_server;
+		listen [::]:80 default_server;
+	
+		add_header X-Served-By ${hostname};
+		root /var/www/html;
+	
+		# Add index.php to the list if you are using PHP
+		index index.html index.htm index.nginx-debian.html;
+	
+		server_name _;
+	
+		location / {
+			# First attempt to serve request as file, then
+			# as directory, then fall back to displaying a 404.
+			try_files \$uri \$uri/ =404;
+		}
 	}
-}
 ",
 }
 
