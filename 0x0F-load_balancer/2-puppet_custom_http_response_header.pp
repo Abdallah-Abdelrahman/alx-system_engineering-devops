55b01c467cee14e06a8731300299e0dfd47cb305
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-05 04:06:31 +0300

    Update 015-2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,32 @@
 # We’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
 
-$header = "add_header X-Served-By \$hostname;"
+node 'default' {
+  # Ensure the package manager is up to date
+  exec { 'apt_update':
+    command => '/usr/bin/apt-get update',
+  }
 
-# Ensure Nginx is installed
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt_update'],
-}
+  # Ensure Nginx is installed
+  package { 'nginx':
+    ensure  => installed,
+    require => Exec['apt_update'],
+  }
 
-file_line {'default':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server {',
-  line    => $header,
-  require => Package['nginx'],
-}
+  # Define the custom response header
+  $header = "\n\tadd_header X-Served-By \$hostname;\n"
 
-service {'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+  # Add the custom header to the Nginx configuration
+  file_line { 'nginx_custom_header':
+    path    => '/etc/nginx/sites-available/default',
+    line    => $header,
+    after   => 'server {',
+    require => Package['nginx'],
+  }
+
+  # Ensure the Nginx service is running
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File_line['nginx_custom_header'],
+  }
 }
