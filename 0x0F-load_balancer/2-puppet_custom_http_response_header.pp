edea6e6a21c7ad0eccfd24d4b92f71ecfdef6a3e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-05 03:06:55 +0300

    Add `match` rule

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,12 @@
 # We’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
 
-$host = exec {'hostname': path => '/bin/hostname'}
 package {'haproxy': ensure => installed}
 
 file_line {'/etc/nginx/sites-enabled/default':
   ensure  => present,
-  after   => 'server {',
-  line    => "add_header X-Served-By ${host};",
+  after   => '^server {',
+  match   => '^server {',
+  line    => "add_header X-Served-By ${hostname};",
   require => Package['nginx'],
 }
 
