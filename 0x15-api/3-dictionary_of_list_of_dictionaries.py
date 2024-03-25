f01078fbcd4fc4b1eb7ace9436d074ab71e00d38
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-25 23:56:07 +0300

    Import all user and corresponding todos

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''script returns information for a given employee ID
+about his/her TODO list progress
+
+Attrs:
+    URL: endpoint to retrive the iformation from
+'''
+from requests.exceptions import HTTPError
+from json import dumps
+from requests import get
+
+URL = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    try:
+        users = get(f'{URL}/users').json()
+        todos = get(f'{URL}/todos').json()
+        data = {}
+
+        for u in users:
+            for t in todos:
+                if u.get('id') == t.get('userId'):
+                    data.update({u.get('id'):
+                                 [
+                                 {'username': u.get('username'),
+                                  'task': t.get('title'),
+                                  'completed': t.get('completed')}
+                                 for t in todos
+                                 ]
+                                 })
+
+        with open('todo_all_employees.json', 'w') as f:
+            f.write(dumps(data))
+
+    except HTTPError as http_err:
+        print(http_err)
+    except Exception as e:
+        print(e)
