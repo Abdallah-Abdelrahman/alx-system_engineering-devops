edbb225b7f8763adeaf5a5ee3556d41e627d9ed8
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-09 21:56:40 +0300

    Remove execption handling

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -23,15 +23,11 @@ def number_of_subscribers(subreddit):
     # Custom User-Agent header to avoid Too Many Requests error
     headers = {'User-Agent': 'Chrome/120.0.0.0'}
 
-    try:
-        resp = get(url, headers=headers, allow_redirects=False)
-        if not (200 <= resp.status_code <= 299):
-            # status is not success
-            return 0
-        data = resp.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
+    resp = get(url, headers=headers, allow_redirects=False)
+    if not (200 <= resp.status_code <= 299):
+        # status is not success
         return 0
-    except Exception:
-        # Return 0 if there's an error
-        return 0
+    data = resp.json()
+    if 'data' in data and 'subscribers' in data['data']:
+        return data['data']['subscribers']
+    return 0
