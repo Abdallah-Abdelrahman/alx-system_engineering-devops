125704fdf8f04e3623148034d3e75a90c1cb3791
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-19 01:16:25 +0300

    Update 0x08-1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,9 @@
 # fix 'holberton' user limit
-exec {'sed -i "/holberton hard/s/5/65535" /etc/security/limit.conf': path => '/bin':/usr/local/bin'}
-exec {'sed -i /holberton soft/s/4/65535 /etc/security/limit.conf': path => '/bin':/usr/local/bin'}
+exec {'sed':
+  command => 'sed -i "/holberton hard/s/5/65535" /etc/security/limit.conf',
+  path    => '/bin:/usr/local/bin'
+}
+exec {'sed':
+  command => 'sed -i "/holberton soft/s/4/65535" /etc/security/limit.conf',
+  path    => '/bin:/usr/local/bin'
+}
