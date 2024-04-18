791b45298b75196a76cbb0ee9bf30811e1f218bf
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-18 20:43:59 +0300

    Update 0x01-0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,2 +1,2 @@
 # reduce the number of failing rquest in nginx server
-exec {'sed -i "s/15/65535/g" /etc/default/nginx': path => '/bin'}
+exec {'sed -i "s/^/\#/g" /etc/default/nginx': path => '/bin'}
